3c02dda871a129a1f989688a8904cd669989e89b
Stefan <oumansour.stefan@gmail.com>
2025-09-26 17:09:08 +0200

    Renderer progress, stuff is being rendered

--- Shaders/common.glsl
+++ Shaders/common.glsl
@@ -55,7 +55,7 @@
 #define DECLARE_PER_FRAME_PARAMS() \
     layout(set=0, binding=0, std140) uniform FrameData { \
         FrameInfo u_frame_info; \
-    }; \
+    }; /*\
     layout(set=0, binding=1, std430) readonly buffer DirectionalLights { \
         DirectionalLight u_directional_lights[]; \
     }; \
@@ -63,7 +63,7 @@
         PointLight u_point_lights[]; \
     }; \
     layout(set=0, binding=3) uniform sampler2D u_brdf_lut ; \
-    layout(set=0, binding=4) uniform sampler2DArray u_shadow_map_noise_texture
+    layout(set=0, binding=4) uniform sampler2DArray u_shadow_map_noise_texture*/
 #endif
 
 #ifdef SHADER_STAGE_COMPUTE
@@ -92,14 +92,14 @@
     layout(set=1, binding=0) uniform Viewpoints { \
         uint u_num_viewpoints; \
         Viewpoint u_viewpoints[Max_Viewpoints]; \
-    }; \
+    }; /*\
     layout(set=1, binding=1) uniform sampler2DArrayShadow u_shadow_maps[Max_Shadow_Maps]; \
     layout(set=1, binding=2) uniform samplerCube u_point_shadow_maps[Max_Point_Shadow_Maps]; \
     layout(set=1, binding=3) uniform sampler2D u_irradiance_map; \
     layout(set=1, binding=4) uniform sampler2D u_environment_map; \
     layout(set=1, binding=5) readonly buffer Clusters { \
         LightCluster u_clusters[]; \
-    }
+    }*/
 #endif
 
 #ifdef SHADER_STAGE_VERTEX
