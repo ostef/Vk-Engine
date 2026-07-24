d5243785cadae9c751c095b316c8b1be416f1b45
Stefan <oumansour.stefan@gmail.com>
2026-07-25 00:51:14 +0200

    Update JoltC.h

--- Modules/JoltBindings/JoltC.h
+++ Modules/JoltBindings/JoltC.h
@@ -255,15 +255,15 @@ enum {
 };
 
 typedef struct JPH_BodyManager_BodyStats {
-    uint32_t mNumBodies;
-    uint32_t mMaxBodies;
-    uint32_t mNumBodiesStatic;
-    uint32_t mNumBodiesDynamic;
-    uint32_t mNumActiveBodiesDynamic;
-    uint32_t mNumBodiesKinematic;
-    uint32_t mNumActiveBodiesKinematic;
-    uint32_t mNumSoftBodies;
-    uint32_t mNumActiveSoftBodies;
+    uint32_t numBodies;
+    uint32_t maxBodies;
+    uint32_t numBodiesStatic;
+    uint32_t numBodiesDynamic;
+    uint32_t numActiveBodiesDynamic;
+    uint32_t numBodiesKinematic;
+    uint32_t numActiveBodiesKinematic;
+    uint32_t numSoftBodies;
+    uint32_t numActiveSoftBodies;
 } JPH_BodyManager_BodyStats;
 
 // Debug renderer
